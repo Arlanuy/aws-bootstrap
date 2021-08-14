ca1b2e47e94ef1462ad29a29222a54b927d3cf02
Arlan Uy <gnhs.com@gmail.com>
2021-08-14 02:01:12 -0500

    Remove http listener from the application

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const { hostname } = require('os');
-const http = require('http');
-const https = require('https'); 
+const https = require('https');
 const fs = require('fs');
 
 const STACK_NAME = process.env.STACK_NAME || "Unknown Stack";
-const port = 8080;
-const httpsPort = 8443; 
-const httpsKey = '../keys/key.pem' 
-const httpsCert = '../keys/cert.pem' 
+const httpsPort = 8443;
+const httpsKey = '../keys/key.pem'
+const httpsCert = '../keys/cert.pem'
 
-if (fs.existsSync(httpsKey) && fs.existsSync(httpsCert)) { 
+if (fs.existsSync(httpsKey) && fs.existsSync(httpsCert)) {
   console.log('Starting https server')
   const message = `Hello HTTPS World from ${hostname()} in ${STACK_NAME}\n`;
   const options = { key: fs.readFileSync(httpsKey), cert: fs.readFileSync(httpsCert) };
-  const server = https.createServer(options, (req, res) => { 
+  const server = https.createServer(options, (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end(message);
@@ -21,15 +19,6 @@ if (fs.existsSync(httpsKey) && fs.existsSync(httpsCert)) {
   server.listen(httpsPort, hostname, () => {
     console.log(`Server running at http://${hostname()}:${httpsPort}/`);
   });
+} else {
+  console.log('Could not find certificate/key');
 }
-
-console.log('Starting http server')
-const message = `Hello HTTP World from ${hostname()} in ${STACK_NAME}\n`;
-const server = http.createServer((req, res) => { 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(message);
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname()}:${port}/`);
-});
