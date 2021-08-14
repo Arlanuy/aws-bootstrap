3d69333121564050fc10b7cf0bee48b70e18bca7
Arlan Uy <gnhs.com@gmail.com>
2021-08-14 01:20:07 -0500

    Add a second instance

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -6,6 +6,13 @@ CLI_PROFILE=uy_arlan
 
 EC2_INSTANCE_TYPE=t2.micro
 
+# Generate a personal access token with repo and admin:repo_hook
+#    permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
 echo -e "\n\n=========== Deploying main.yml ==========="
 
 aws cloudformation deploy \
@@ -16,4 +23,16 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \ 
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+# If the deploy succeeded, show the DNS name of the created instance
+if [ $? -eq 0 ]; then
+  aws cloudformation list-exports \
+    --profile awsbootstrap \
+    --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
+fi
