8210884418888f0589538f493883bf6536d4a963
Arlan Uy <gnhs.com@gmail.com>
2021-08-14 01:41:01 -0500

    Split out staging nested stack

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -12,6 +12,7 @@ GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
 GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
 GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
 GH_BRANCH=master
+CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
 
 echo -e "\n\n=========== Deploying main.yml ==========="
 
@@ -30,9 +31,27 @@ aws cloudformation deploy \
     GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
-# If the deploy succeeded, show the DNS name of the created instance
-if [ $? -eq 0 ]; then
-  aws cloudformation list-exports \
-    --profile awsbootstrap \
-    --query "Exports[?ends_with(Name,'LBEndpoint')].Value" 
+ # If the deploy succeeded, show the DNS name of the endpoints
+ if [ $? -eq 0 ]; then
+   aws cloudformation list-exports \
+     --profile awsbootstrap \
+     --query "Exports[?ends_with(Name,'LBEndpoint')].Value"
+ fi
+
+
+# Package up CloudFormation templates into an S3 bucket
+echo -e "\n\n=========== Packaging main.yml ==========="
+mkdir -p ./cfn_output
+
+PACKAGE_ERR="$(aws cloudformation package \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --template main.yml \
+  --s3-bucket $CFN_BUCKET \
+  --output-template-file ./cfn_output/main.yml 2>&1)"
+
+if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+  echo "ERROR while running 'aws cloudformation package' command:"
+  echo $PACKAGE_ERR
+  exit 1
 fi
